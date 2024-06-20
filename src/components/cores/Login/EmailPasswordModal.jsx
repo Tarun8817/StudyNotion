c4d382d7d321e4b90afa2bc4ec9f57dc6fb82e38
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-06-20 14:00:26 +0530

    update

--- src/components/cores/Login/EmailPasswordModal.jsx
+++ src/components/cores/Login/EmailPasswordModal.jsx
@@ -18,7 +18,7 @@ const DummyEmailModal = () => {
         onClick={openModal}
         className="text-black font-medium bg-[#ffffff] rounded-full px-2 cursor-pointer animate-wave"
       >
-        Login Detail
+        Login Details
         
       </button>
       <Modal
